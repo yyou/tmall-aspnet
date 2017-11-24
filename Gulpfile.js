ad13aa81b601498137bfd5357de9e81a13377da2
yyou <yy_318@hotmail.com>
2017-11-24 18:49:02 +1100

    Added gulp-webserver and gulp-livereload. Modified category-carousel part.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+var webserver = require('gulp-webserver');
 
-gulp.task('styles', function() {
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('sass', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./style/'))
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+gulp.task('watch',function() {
+    gulp.watch('sass/**/*.scss', ['sass']);
+    //gulp.watch('*.html', ['html']);
 });
+
+gulp.task('default', ['webserver', 'watch']);
